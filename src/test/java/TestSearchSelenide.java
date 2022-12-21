26ff4891156ae01fc8904888573858bc3dbbacfe
Adelina <adelina_usmanova@mail.ru>
2022-12-22 00:35:04 +0300

    added build.gradle

---
+++ src/test/java/TestSearchSelenide.java
@@ -0,0 +1,23 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
+
+public class TestSearchSelenide {
+
+
+    @Test
+    void SearchExample() {
+        Configuration.holdBrowserOpen = true;
+
+        open("https://github.com/");
+        $("#wiki-tab").click();
+        $("#wiki-pages-box button").click();
+        $("#wiki-pages-box").shouldHave(text("SoftAssertions"));
+        $("#wiki-pages-box").$(byText("SoftAssertions")).click();
+        $(".markdown-body").shouldHave(text("Using JUnit5 extend test class"));
+    }
+}
+
